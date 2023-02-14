30b49bb00c1c519a8f2a24ee31c683bdd471bf9d
DenisseRincon <info@q-antica.com>
2023-02-14 14:38:05 -0500

    commit 14th feb

---
+++ app/src/main/java/qantica/app/appgps/view/EvidenceActivity.java
@@ -0,0 +1,14 @@
+package qantica.app.appgps.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EvidenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evidence);
+    }
+}

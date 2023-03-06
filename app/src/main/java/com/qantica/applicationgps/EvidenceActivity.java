da5ba33c3da2053597aa14979f5d43047407bae3
DenisseRincon <info@q-antica.com>
2023-03-06 14:24:53 -0500

    Update 6th march

--- app/src/main/java/com/qantica/applicationgps/EvidenceActivity.java
+++ app/src/main/java/com/qantica/applicationgps/EvidenceActivity.java
@@ -3,18 +3,46 @@ package com.qantica.applicationgps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 
 public class EvidenceActivity extends AppCompatActivity {
 
+    ImageView evidenceCamera;
+    ImageView evidenceView;
     View view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evidence);
+
+        evidenceCamera = findViewById(R.id.evidenceCamera);
+        evidenceView = findViewById(R.id.evidenceView);
+
+        evidenceCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCamera();
+            }
+        });
+
+    }
+    private void openCamera(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1);
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imgBitmap = (Bitmap) extras.get("data");
+            evidenceView.setImageBitmap(imgBitmap);
+        }
+    }
     public void regresar(View view){
         this.view = view;
         Intent enter = new Intent(this, LocationActivity.class);
--- app/src/main/java/com/qantica/applicationgps/FormActivity.java
+++ app/src/main/java/com/qantica/applicationgps/FormActivity.java
@@ -3,16 +3,44 @@ package com.qantica.applicationgps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 
 public class FormActivity extends AppCompatActivity {
 
+    ImageView evidenceCamera2;
+    ImageView fotoForm;
     View view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+        evidenceCamera2 = findViewById(R.id.evidenceCamera2);
+        fotoForm = findViewById(R.id.fotoForm);
+
+        evidenceCamera2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCamera();
+            }
+        });
+    }
+
+    private void openCamera(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imgBitmap = (Bitmap) extras.get("data");
+            fotoForm.setImageBitmap(imgBitmap);
+        }
     }
     public void regresar(View view){
         this.view = view;
